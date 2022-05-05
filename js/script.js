a0e9bd5c320419057fe2fef0e4d8c21a5fac2e96
StefanoMast <ste.mastrantonio@gmail.com>
2022-05-05 18:18:24 +0200

    add bonus

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ const app = new Vue(
     {
         el: '#root', //vue deve lavorare all'interno del contenitore root
         data: {
-            message: 'Ciao, è la prima volta che uso Vue.js!',
+            title: 'Ciao, è la prima volta che uso Vue.js!',
             //all'interno di una proprietà che si chiama title abbiamo il titolo
+            myImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/FujiSunriseKawaguchiko2025WP.jpg/260px-FujiSunriseKawaguchiko2025WP.jpg"
         }
     });
 
