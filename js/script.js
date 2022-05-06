2961c03b202c4a81a317ef261edacc97a4f2968d
StefanoMast <ste.mastrantonio@gmail.com>
2022-05-06 09:36:37 +0200

    add img

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-
+Vue.config.devtools = true;
 const app = new Vue(
     {
         el: '#root', //vue deve lavorare all'interno del contenitore root
         data: {
             title: 'Ciao, è la prima volta che uso Vue.js!',
             //all'interno di una proprietà che si chiama title abbiamo il titolo
-            myImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/FujiSunriseKawaguchiko2025WP.jpg/260px-FujiSunriseKawaguchiko2025WP.jpg"
+            myImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/FujiSunriseKawaguchiko2025WP.jpg/260px-FujiSunriseKawaguchiko2025WP.jpg'
         }
-    });
+    }
+);
 
